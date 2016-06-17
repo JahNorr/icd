e7b38c70080516e4fb310cb9bf9dbeac3edff7e9
jahNorr <John.Orr@doh.vi.gov>
2016-06-17 13:22:01 -0400

    initial commit

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' ICD10 Recode datasets for cause of death
+#'
+#' Datasets containing the recoding of ICD10 codes into smaller sets.
+#'
+#'   For purposes of publishing data on causes of death for 1999 and beyond,
+#'   NCHS groups the data for selected ICD-10 cause of death codes.
+#'   Consequently each ICD-10 cause of death code is provided with a “recode”
+#'   to indicate which group or data category a particular cause of death is associated with.
+#'   The follwing recode sets are avaiable ...
+#'   recode39 - 39 recodes of selected causes of death
+#'   recode113 - 113 recodes of selected causes of death
+#'
+#'
+#' \itemize{
+#'   \item recode price in US dollars (\$326--\$18,823)
+#'   \item title weight of the diamond (0.2--5.01)
+#'#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name recode
+#' @usage data(diamonds)
+#' @format A data frame with 2 variables
+NULL
